ddf2fc5b7924e2f26cf13220aa78906f567c7b3b
Julian Seuffert <106593938+jseuffert90@users.noreply.github.com>
2022-11-04 15:27:28 +0100

    More features for mapviewer.py
    - supports writing into output file
    - supports adjusting dpi and font size
    - supports adjusting figure width and height
    - supports choosing the color map

--- mapviewer.py
+++ mapviewer.py
@@ -8,20 +8,31 @@ import math
 import os
 import sys
 
+from matplotlib import rc
 import matplotlib.pyplot as plt
 import numpy as np
 import tifffile
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Displays multiple map files')
+    # positional arguments
     parser.add_argument('file', type=str, nargs="+", help="input files")
+
+    # keyword arguments
+    parser.add_argument('--cmap', '-c', type=str, default='gnuplot2', help="matplotlib colormap")
+    parser.add_argument('--dpi', '-d', type=int, help="output file dpi")
+    parser.add_argument('--figwidth', '-w', type=float, help="figure width in cm")
+    parser.add_argument('--figheight', type=float, help="figure height in cm")
+    parser.add_argument('--fontsize', '-f', type=int, default=12, help="font size")
     parser.add_argument('--loglevel', '-l', \
             choices=['critical', 'error', 'warning', 'info', 'debug'], \
             default="warning", type=str, help="set the log level")
+    parser.add_argument('--names', '-n', type=str, nargs="+", help="title or name for each subfigure (default: file name)")
+    parser.add_argument('--outfile', '-o', type=str, help="output file (if set, no plots are shown on the display)")
     parser.add_argument('--vmax', type=float, \
-            help="maximum value to be in color range (default: max. observable value of all input files")
+            help="maximum value to be in color range (default: max. observable value of all input files)")
     parser.add_argument('--vmin', type=float, \
-            help="minimum value to be in color range (default: min. observable value of all input files")
+            help="minimum value to be in color range (default: min. observable value of all input files)")
 
     args = parser.parse_args()
     
@@ -31,7 +42,25 @@ if __name__ == "__main__":
     stream = logging.StreamHandler()
     stream.setLevel(log_level)
     logger.addHandler(stream)
-    
+
+    # cm -> inch
+    figure_width = args.figwidth / 2.54 if args.figwidth is not None else None
+    figure_height = args.figheight / 2.54 if args.figheight is not None else None
+
+    if figure_width is None and figure_height is not None:
+        figure_width = figure_height
+    elif figure_width is not None and figure_height is None:
+        figure_height = figure_width
+   
+    font = {'family' : 'serif',
+            'serif'  : ['Computer Modern'],
+            'size'   : args.fontsize}
+    rc('font', **font)
+    rc('text', usetex=True)
+
+    # if args.fontsize is not None:
+    #     plt.rcParams.update({'font.size': args.fontsize})
+
     images = []
     for f in args.file:
         _, ext = os.path.splitext(f)
@@ -73,7 +102,10 @@ if __name__ == "__main__":
     logger.debug(f"{n_rows=}")
     logger.debug(f"{n_cols=}")
 
-    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols)
+    if figure_width is not None and figure_height is not None:
+        fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(figure_width, figure_height))
+    else:
+        fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols)
     n_fields = n_rows * n_cols
 
     if n_fields > 1:
@@ -83,13 +115,19 @@ if __name__ == "__main__":
 
     for i, axis in enumerate(axes_ravel):
         if i < num_images:
-            handle = axis.imshow(images[i], vmin=min_val, vmax=max_val)
-            t = os.path.basename(args.file[i])
+            handle = axis.imshow(images[i], vmin=min_val, vmax=max_val, cmap=args.cmap)
+            if args.names is not None and len(args.names) > i:
+                t = args.names[i]
+            else:
+                t = os.path.basename(args.file[i])
             axis.set_title(t)
         else:
             fig.delaxes(axis)
    
     fig.colorbar(handle, ax=axes_ravel)
 
-    plt.show()
+    if args.outfile is None:
+        plt.show()
+    else:
+        plt.savefig(args.outfile, dpi=(args.dpi if args.dpi is not None else 'figure'))
 
