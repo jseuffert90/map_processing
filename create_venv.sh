d6f507855470ff595b895884c270d30645e196c6
Julian Seuffert <julian.seuffert@etit.tu-chemnitz.de>
2023-11-21 15:37:39 +0100

    Use OpenEXR v1.3.8 (v1.3.9 is buggy on Ubuntu 22.04)

--- create_venv.sh
+++ create_venv.sh
@@ -26,8 +26,10 @@ fi
 sed -i "s#PATH=\"\$VIRTUAL_ENV#PATH=\"$PWD:\$VIRTUAL_ENV#g" $virt_env/bin/activate
 source "$virt_env/bin/activate"
 
+# OpenEXR v1.3.9 does not work on Ubuntu 22.04! --> v1.3.8 used
+
 python -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu && \
-python -m pip install pillow matplotlib tifffile tqdm tensorboard openexr numpy opencv-python pyqt5 imagecodecs && \
+python -m pip install pillow matplotlib tifffile tqdm tensorboard OpenEXR==1.3.8 numpy opencv-python pyqt5 imagecodecs && \
 abortOnFailure
 
 python -m pip install open3d
